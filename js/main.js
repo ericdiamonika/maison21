6501be917654c8faa19d0a332c10465e56d84885
doriancolin <dorian.colin@cotenature.com>
2019-06-12 15:49:24 +0200

    modifs

--- js/main.js
+++ js/main.js
@@ -1,30 +1,24 @@
-/*
-document.ready = function () {
-    var frameNumber = 0, // start video at frame 0
-        // lower numbers = faster playback
-        playbackConst = 500,
-        // get page height from video duration
-        setHeight = document.getElementById("set-height"),
-        // select video element
-        vid = document.getElementById('v0');
-// var vid = $('#v0')[0]; // jquery option
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-// dynamically set the page height according to video length
-    vid.addEventListener('loadedmetadata', function () {
-        setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
-    });
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
+      // Store hash
+      var hash = this.hash;
 
-// Use requestAnimationFrame for smooth playback
-    function scrollPlay() {
-        var frameNumber = window.pageYOffset / playbackConst;
-        vid.currentTime = frameNumber;
-        window.requestAnimationFrame(scrollPlay);
-    }
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
 
-    window.requestAnimationFrame(scrollPlay);
-
-
-}
-
-*/
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
